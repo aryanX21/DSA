1b99860619c32fba487d86114b4df9c652af180e
aryanX21 <2025.aryanprasad@gmail.com>
2026-06-11 02:36:32 +0530

    Add reverse array program

---
+++ 01_Arrays/03_Reverse_Array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size){
+    int start = 0, end = size - 1;
+
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+
+    int size;
+    cout<<"\nEnter size of array: ";
+    cin>>size;
+
+    int arr[size];
+
+    cout<<"\nEnter elements: ";
+
+    for(int i = 0; i < size; i++){
+        cin>>arr[i];
+    }    
+
+    reverseArray(arr, size);
+
+    cout<<"Reverse: ";
+
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
