efb2073303a415dc182b74db1c5d8e32593cc54b
aryanX21 <2025.aryanprasad@gmail.com>
2026-06-13 18:35:28 +0530

    Add syntax and basics of vector

---
+++ 01_Arrays/02_Vectors/01_Syntax.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> vec1;  // empty vector 
+
+    vector<int> vec2 = {1,2,3,4,5}; 
+
+    cout<<vec2[0]<<endl;
+    cout<<vec2[3]<<endl;
+
+    //for each loop
+    
+    //       variable name 
+    for( int i : vec2){             // in this loop 'i' gives the value at that index
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+
+    vector<int> vec3 (4,0);  /* here 4 is the size of vector and 0 is used to 
+                                 initiallize alll 8 */
+
+    for( int i : vec3){             // in this loop i gives the value at that index
+        cout<<i<<" ";
+    }
+
+   return 0;
+
+}
